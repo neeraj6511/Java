71ff6df14b602f7efae729fc4b7df6d28e51a5ff
neeraj6511 <101538685+neeraj6511@users.noreply.github.com>
2022-10-01 00:26:31 +0530

    Add files via upload

---
+++ Subtract.java
@@ -0,0 +1,13 @@
+
+public class Sub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int counter =50;
+		counter = counter -5;
+		 
+		System.out.println("The Number of  Subtract is " + counter );
+
+	}
+
+}
